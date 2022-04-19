8ca06fd34cd0beec16a46f5f4e80c83d40897e23
Luis Perez <luiseperezp.10@gmail.com>
2022-04-19 13:25:06 -0500

    feat(component): customize footer

--- src/app/core/components/admin/footer/footer.component.ts
+++ src/app/core/components/admin/footer/footer.component.ts
@@ -1,12 +1,79 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss'],
+	selector: 'app-footer',
+	templateUrl: './footer.component.html',
+	styleUrls: [ './footer.component.scss' ]
 })
-export class FooterComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class FooterComponent {
+	links = [
+		{
+			title: 'Acerca de',
+			linkCollections: [
+				{
+					label: 'GDSC',
+					url: '#'
+				},
+				{
+					label: 'GDSC UNI',
+					url: '#'
+				},
+				{
+					label: 'GDSC UNICA',
+					url: '#'
+				},
+				{
+					label: 'GDSC UTP',
+					url: '#'
+				}
+			]
+		},
+		{
+			title: 'Síguenos en',
+			linkCollections: [
+				{
+					label: 'Facebook',
+					url: '#'
+				},
+				{
+					label: 'Twitter',
+					url: '#'
+				},
+				{
+					label: 'Instagram',
+					url: '#'
+				},
+				{
+					label: 'Discord',
+					url: '#'
+				}
+			]
+		},
+		{
+			title: 'Recursos',
+			linkCollections: [
+				{
+					label: 'Escríbenos',
+					url: 'mailto:gdsc.peru@gmail.com'
+				},
+				{
+					label: 'Código de conducta',
+					url: '#'
+				},
+				{
+					label: 'Políticas de Privacidad',
+					url: '#'
+				},
+				{
+					label: 'Call for Speakers',
+					url: '#'
+				},
+				{
+					label: 'FAQs',
+					url: '#'
+				}
+			]
+		}
+	];
+	constructor() {}
 }
