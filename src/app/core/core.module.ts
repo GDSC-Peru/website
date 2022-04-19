43740982c075c0b72696e846a005e0502cc01fdc
lperezp <luiseperezp.10@gmail.com>
2022-04-18 22:25:38 -0500

    feat(app): Remove auth module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,16 +4,15 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 
 import { AdminComponent } from './layouts/admin/admin.component';
-import { AuthComponent } from './layouts/auth/auth.component';
 import { FooterComponent } from './components/admin/footer/footer.component';
 import { HeaderComponent } from './components/admin/header/header.component';
 import { SidebarComponent } from './components/admin/sidebar/sidebar.component';
 
-const CORE_COMPONENTS = [AuthComponent, AdminComponent, HeaderComponent, FooterComponent, SidebarComponent];
+const CORE_COMPONENTS = [AdminComponent, HeaderComponent, FooterComponent, SidebarComponent];
 const CORE_MODULES = [CommonModule, SharedModule];
 
 @NgModule({
-  declarations: [...CORE_COMPONENTS, FooterComponent, SidebarComponent, HeaderComponent, AuthComponent],
+  declarations: [...CORE_COMPONENTS, FooterComponent, SidebarComponent, HeaderComponent ],
   imports: [...CORE_MODULES],
 })
 export class CoreModule {}
