bb10544f57860b91699a29f57a7c99ee181dffa0
Luis Perez <luiseperezp.10@gmail.com>
2022-04-19 11:19:10 -0500

    feat(pages): add speakers page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { SpeakersComponent } from './pages/speakers/speakers.component';
 
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'speakers',
+    component: SpeakersComponent,
     pathMatch: 'full'
   },
 ];
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { CoreModule } from './../core/core.module';
+import { SpeakersComponent } from './speakers/speakers.component';
 
-const PAGES = [HomeComponent];
+const PAGES = [HomeComponent, SpeakersComponent];
 
 
 @NgModule({
   declarations: [...PAGES],
   imports: [
-  CommonModule,
+    CommonModule,
     CoreModule
   ]
 })
